d8c2bf561aa2d6251c2d847690e6dbcd1d499123
Chris Vinson <vinsonvendor@gmail.com>
2021-03-30 01:13:56 +0000

    Untrack files in .gitignore

--- cv_personal_portfolio/settings.py
+++ cv_personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cmm(9qen_w4h(_5sbjl0mbvf(z@73hj%86nm0*=p)tfuv64179'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ceevee830.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
